c426910a78c581461be77fa2a6312fc2689f4993
KevinCay <kcayc@miumg.edu.gt>
2024-10-02 19:00:55 -0600

    first commit

---
+++ app/config/env.js
@@ -0,0 +1,16 @@
+
+const env = {
+  database: 'antigua_umg2024_2103_e248',
+  username: 'antigua_umg2024_2103_e248_user',
+  password: 'GneAtTocEf776SxCABuqfrRs4iwyuKOK',
+  host: 'dpg-cruu1maj1k6c73fpph5g-a',
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
+
+module.exports = env;
